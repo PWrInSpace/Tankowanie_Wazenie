31f5ae74e219333a9aed2d4dc07446be299f5ed9
K00bek <kubson153@gmail.com>
2021-05-27 16:02:27 +0200

    final fro  wpt, worki ng enigne igniter to fix

--- R4_STM_CODE/Core/Src/Ingniter.c
+++ R4_STM_CODE/Core/Src/Ingniter.c
@@ -1,9 +1,9 @@
 #include "Igniter.h"
 
 //make struct 'Igniter', fill it, return pointer to it    (names of args start with '_' sign)
-Igniter* igniter_init(GPIO_TypeDef* _GPIO_PORT_IGNITER, uint16_t _PIN_IGNITER, GPIO_TypeDef* _GPIO_PORT_TEST_CON, uint16_t _PIN_TEST_CON)
-{
-	Igniter* I = malloc(sizeof(Igniter));	 //allocate space for struct
+Igniter* igniter_init(GPIO_TypeDef *_GPIO_PORT_IGNITER, uint16_t _PIN_IGNITER,
+		GPIO_TypeDef *_GPIO_PORT_TEST_CON, uint16_t _PIN_TEST_CON) {
+	Igniter *I = malloc(sizeof(Igniter));	 //allocate space for struct
 	//fill whole struct
 	I->GPIO_PORT_IGNITER = _GPIO_PORT_IGNITER;
 	I->PIN_IGNITER = _PIN_IGNITER;
@@ -12,27 +12,26 @@ Igniter* igniter_init(GPIO_TypeDef* _GPIO_PORT_IGNITER, uint16_t _PIN_IGNITER, G
 	return I;
 }
 
-bool igniter_is_connected(Igniter* Igniter){
-	if(HAL_GPIO_ReadPin(Igniter->GPIO_PORT_TEST_CON, Igniter->PIN_TEST_CON) == 1)
+bool igniter_is_connected(Igniter *Igniter) {
+	if (HAL_GPIO_ReadPin(Igniter->GPIO_PORT_TEST_CON, Igniter->PIN_TEST_CON)
+			== 1)
 		return 1;
 	else
 		return 0;
 }
 
-bool igniter_FIRE(Igniter* Igniter){
+bool igniter_FIRE(Igniter *Igniter) {
 	HAL_GPIO_WritePin(Igniter->GPIO_PORT_IGNITER, Igniter->PIN_IGNITER, 1);
-	if(igniter_is_connected(Igniter) == 1)
-	{
+	if (igniter_is_connected(Igniter) == 1) {
 		return 1;
-	}
-	else
+	} else
 		return 0;
 }
 
-
-void toggle_Mosfet(){
+void toggle_Mosfet() {
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
-	HAL_Delay(200);
+
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+
 }
 
