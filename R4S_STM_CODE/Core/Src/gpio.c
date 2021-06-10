9c13e9473ce84e4165a6c4f4fafbe7990c232140
KowalDrzo <248959@student.pwr.edu.pl>
2021-06-10 20:21:19 +0200

    Poprawki z wpt - całość działa

--- R4S_STM_CODE/Core/Src/gpio.c
+++ R4S_STM_CODE/Core/Src/gpio.c
@@ -75,6 +75,12 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(IGN_FIRE_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = CONNECTION_TEST_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(CONNECTION_TEST_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 2 */
