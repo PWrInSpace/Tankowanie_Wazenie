d974f87ca4c1bd4925424490bfab7a37d361d2a3
FilipKulisiewicz <73996991+FilipKulisiewicz@users.noreply.github.com>
2021-08-06 16:36:53 +0200

    add geters and setters

--- R4_STM_CODE/Core/Inc/voltmeter.hh
+++ R4_STM_CODE/Core/Inc/voltmeter.hh
@@ -1,11 +1,17 @@
 #ifndef INC_VOLTMETER_HH_
 #define INC_VOLTMETER_HH_
 
+#include "gpio.h"
 
 class Voltmeter{
-
+	GPIO_TypeDef *Analog_gpio;
+	uint16_t Analog_pin;
+	int16_t VoltageDividerRatio;
+public:
+	Voltmeter(GPIO_TypeDef *_Analog_gpio, uint16_t _Analog_pin, int16_t _VoltageDividerRatio);
+	void SetVoltageDividerRatio(int16_t NewVoltageDividerRatio);
+	int16_t GetVoltageDividerRatio();
+	int16_t GetBatteryVoltage();
 };
-
-
 
 #endif /* INC_VOLTMETER_HH_ */
--- R4_STM_CODE/Core/Src/voltmeter.cpp
+++ R4_STM_CODE/Core/Src/voltmeter.cpp
@@ -1,1 +1,16 @@
 #include "voltmeter.hh"
+
+Voltmeter::Voltmeter(GPIO_TypeDef *_Analog_gpio, uint16_t _Analog_pin, int16_t _VoltageDividerRatio)
+: Analog_gpio(_Analog_gpio), Analog_pin(_Analog_pin), VoltageDividerRatio(_VoltageDividerRatio) {}
+
+void Voltmeter::SetVoltageDividerRatio(int16_t NewVoltageDividerRatio){
+	VoltageDividerRatio = NewVoltageDividerRatio;
+}
+
+int16_t Voltmeter::GetVoltageDividerRatio(){
+	return VoltageDividerRatio;
+}
+
+int16_t Voltmeter::GetBatteryVoltage(){
+
+}
