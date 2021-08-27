3534bd98b5fd1466500264bfb500f180f90ea5cb
filip_kuli <259316@student.pwr.edu.pl>
2021-08-27 08:41:29 -0700

    old main

--- R4_STM_CODE/Core/Src/main.cpp
+++ R4_STM_CODE/Core/Src/main.cpp
@@ -70,8 +70,6 @@ volatile int32_t buf = -1, buf2 = -1, buf3 = -1;
 char dataIn[30];
 char dataOut[30];
 Xbee communication;
-HX711 RocketWeight(HX1_SDA_GPIO_Port, HX1_SDA_Pin, HX1_SCL_GPIO_Port, HX1_SCL_Pin);
-HX711 TankWeight(HX2_SDA_GPIO_Port, HX2_SDA_Pin, HX2_SCL_GPIO_Port, HX2_SCL_Pin);
 Voltmeter VM(&hadc1, 1);
 Motor FillMotor(FILL_OPEN_GPIO_Port, FILL_OPEN_Pin,	FILL_CLOSE_GPIO_Port, FILL_CLOSE_Pin, &htim4, TIM_CHANNEL_3,
 				FILL_OPEN_LIMIT_SW_GPIO_Port, FILL_OPEN_LIMIT_SW_Pin,	FILL_CLOSE_LIMIT_SW_GPIO_Port, FILL_CLOSE_LIMIT_SW_Pin);
@@ -151,7 +149,8 @@ int main(void)
 
 	/* USER CODE BEGIN WHILE */
 
-
+	HX711 RocketWeight(HX1_SDA_GPIO_Port, HX1_SDA_Pin, HX1_SCL_GPIO_Port, HX1_SCL_Pin);
+	HX711 TankWeight(HX2_SDA_GPIO_Port, HX2_SDA_Pin, HX2_SCL_GPIO_Port, HX2_SCL_Pin);
 
 	RocketWeight.initialCalibration(200);
 	while(1){
