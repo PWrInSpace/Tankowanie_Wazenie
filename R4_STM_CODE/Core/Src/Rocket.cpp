5432c2df19ae8b8b15335c34c857587a97a1f329
filip_kuli <259316@student.pwr.edu.pl>
2021-08-30 10:07:21 -0700

    data from rocket

--- R4_STM_CODE/Core/Src/Rocket.cpp
+++ R4_STM_CODE/Core/Src/Rocket.cpp
@@ -1,10 +1,13 @@
 #include "Rocket.hh"
 
-Rocket::Rocket(std::shared_ptr<Motor> _FillMotor, std::shared_ptr<Motor> _DeprMotor, std::shared_ptr<Motor> _QDMotor, std::shared_ptr<Igniter> _igniter){
+Rocket::Rocket(std::shared_ptr<Motor> _FillMotor, std::shared_ptr<Motor> _DeprMotor, std::shared_ptr<Motor> _QDMotor,
+			   std::shared_ptr<Igniter> _igniter,	std::shared_ptr<HX711> _RocketWeight, std::shared_ptr<HX711> _TankWeight){
 	FillMotor =_FillMotor;
 	DeprMotor =_DeprMotor;
 	QDMotor =_QDMotor;
 	igniter = _igniter;
+	RocketWeight = _RocketWeight;
+	TankWeight = _TankWeight;
 	currState = Init;
 }
 	
@@ -12,16 +15,53 @@ uint16_t Rocket::getCurrState(){
 	return currState;
 }
 
-//
 void Rocket::comandHandler(std::string comand){
-	if(comand.substr(0, 4) == "STAT")
+	if(comand.substr(0, 4) == "STAT") //
 		currState = (state)(((int) (comand[7])) - 48);
-	else if(comand.substr(0, 4) == "DSTA" && currState == Countdown)
+	else if(comand.substr(0, 4) == "DSTA" && currState == Countdown)  //FIRE
 		igniter->FIRE();
-	else if(comand.substr(0, 3) == "DWC"){
+	else if(comand.substr(0, 3) == "DWC"){ //calibration
 		if(comand.substr(3, 1) == "R")
-			;
-		else if(comand.substr(3, 1) == "0")
-			;
+			RocketWeight->initialCalibration(std::stoi(comand.substr(5, 6)));
+		else if(comand.substr(3, 1) == "T"){
+			TankWeight->initialCalibration(std::stoi(comand.substr(5, 6)));
+		}
 	}
+	else if(comand.substr(0, 3) == "DWO"){	//wagi
+		if(comand.substr(3, 1) == "R")
+			RocketWeight->addToOffset(std::stoi(comand.substr(5, 8)));
+		else if(comand.substr(3, 1) == "T")
+			TankWeight->addToOffset(std::stoi(comand.substr(5, 8)));
+	}
+	else if(comand.substr(0, 3) == "DWR"){
+		if(comand.substr(3, 1) == "R")
+			RocketWeight->setBitsToGramRatio(std::stoi(comand.substr(5, 8)));
+		else if(comand.substr(3, 1) == "T")
+			TankWeight->setBitsToGramRatio(std::stoi(comand.substr(5, 8)));
+	}
+	else if(comand.substr(0, 2) == "DZ"){	//zawory
+		if(comand.substr(2, 1) == "T")
+			FillMotor->handleComand(std::stoi(comand.substr(3, 1)));
+		else if(comand.substr(2, 1) == "O")
+			DeprMotor->handleComand(std::stoi(comand.substr(3, 1)));
+		else if(comand.substr(2, 1) == "Q")
+			QDMotor->handleComand(std::stoi(comand.substr(3, 1)));
+	}
+}
+
+std::string Rocket::getInfo(){ //not tested
+	std::string tmp(std::to_string(currState));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(igniter->isConnected()));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(FillMotor->getStatus()));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(DeprMotor->getStatus()));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(QDMotor->getStatus()));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(RocketWeight->getWeigthInGramsWithOffset()));
+	tmp.insert(tmp.length(), ";");
+	tmp.insert(tmp.length(), std::to_string(TankWeight->getWeigthInGramsWithOffset()));
+	return tmp;
 }
