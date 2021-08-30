da6a90f4885769811db11c94b8d02ea96c08c4ab
filip_kuli <259316@student.pwr.edu.pl>
2021-08-30 10:43:18 -0700

    add Rocket class

--- R4_STM_CODE/Core/Inc/Rocket.hh
+++ R4_STM_CODE/Core/Inc/Rocket.hh
@@ -1,5 +1,6 @@
 #include "L298.hh"
 #include "Igniter.hh"
+#include "hx711.hh"
 #include "string"
 #include "memory"
 
@@ -17,9 +18,14 @@ class Rocket{
 	std::shared_ptr<Motor> DeprMotor;
 	std::shared_ptr<Motor> QDMotor;
 	std::shared_ptr<Igniter> igniter;
+	std::shared_ptr<HX711> RocketWeight;
+	std::shared_ptr<HX711> TankWeight;
 	volatile state currState;
 public:
-	Rocket(std::shared_ptr<Motor> _FillMotor, std::shared_ptr<Motor> _DeprMotor, std::shared_ptr<Motor> _QDMotor,  	std::shared_ptr<Igniter> _igniter);
+	Rocket() = default;
+	Rocket(std::shared_ptr<Motor> _FillMotor, std::shared_ptr<Motor> _DeprMotor, std::shared_ptr<Motor> _QDMotor,
+		   std::shared_ptr<Igniter> _igniter, std::shared_ptr<HX711> _RocketWeight, std::shared_ptr<HX711> _TankWeight);
 	uint16_t getCurrState();
+	std::string getInfo();
 	void comandHandler(std::string comand);
 };
