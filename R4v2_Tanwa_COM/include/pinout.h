c2db6f98ac6e113fd111d3c71da166530ec52695
jakub-zaton <kuba.zaton@interia.pl>
2022-04-18 22:38:27 +0200

    added pinout

--- R4v2_Tanwa_COM/include/pinout.h
+++ R4v2_Tanwa_COM/include/pinout.h
@@ -1,12 +1,59 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
-#define MOSI      23
-#define MISO      19
-#define SCK       18
-#define SD_CS     17
-#define LORA_CS   5
-#define LORA_RS   4
-#define LORA_D0   39
+#include "FreeRTOS.h"
+#include "Arduino.h"
+
+// igniters
+#define IGN_TEST_CON_1 34
+#define IGN_TEST_CON_2 35
+#define FIRE1 27
+#define FIRE2 26
+#define ARM 32
+
+#define VOLTAGE_MEASURE 36
+
+//LORA
+#define LORA_D0 39
+#define LORA_CS 5
+#define LORA_RS 4
+
+//BUZZER & SPEAKER
+#define BUZZER 33
+#define SPEAKER 25
+
+//HX711
+#define HX1_SDA 15
+#define HX1_SCL 12
+#define HX2_SDA 14
+#define HX2_SCL 12
+
+//RESET
+#define RST 13
+
+//SPI
+#define SPI_MOSI 23
+#define SPI_MISO 19
+#define SPI_SCK 18
+
+//I2C   
+#define I2C_SCL 22
+#define I2C_SDA 21
+
+//SD CHIP SELECT
+#define SD_CS 17
+
+#define TX 1
+#define RX 3
+
+//CAMERA 
+#define RUNCAM 16
+
+#define BOOT 0 //????????
+
+
+void beepBoop(int delay, int times);
+float voltageMeasure(uint8_t batteryPin, const float &reverseDividerVal);
+void pinInit();
 
 #endif
---
+++ R4v2_Tanwa_COM/src/pinout.cpp
@@ -0,0 +1,59 @@
+#include "pinout.h"
+#include <Wire.h>
+
+float voltageMeasure(uint8_t batteryPin)
+{
+    // reverseDividerVal = (R1 + R2) / R2 = 58/11
+    return (analogRead(batteryPin) / 4095) * 3.3 * (58/11);
+}
+
+
+void beepBoop(int time, int howManyTimes){
+    uint32_t delay = time / howManyTimes;
+    for(int i=0; i < howManyTimes; ++i){
+        digitalWrite(BUZZER, 1);
+        vTaskDelay(delay / portTICK_PERIOD_MS);
+        digitalWrite(BUZZER, 0);
+        vTaskDelay(delay / portTICK_PERIOD_MS);
+    }
+}
+
+
+void pinInit()
+{
+    //igniter
+    pinMode(IGN_TEST_CON_1, INPUT); 
+    pinMode(IGN_TEST_CON_2, INPUT);
+    pinMode(ARM, OUTPUT);
+    pinMode(FIRE1, OUTPUT);
+    pinMode(FIRE2, OUTPUT);
+    digitalWrite(ARM, LOW);
+    digitalWrite(FIRE1, LOW);
+    digitalWrite(FIRE2, LOW);
+
+    //VOLTAGE
+    pinMode(VOLTAGE_MEASURE, INPUT);
+
+    //LORA
+
+
+    //BUZZER & SPEAKER
+    pinMode(BUZZER, OUTPUT);
+    digitalWrite(BUZZER, LOW);
+    pinMode(SPEAKER, OUTPUT);
+    digitalWrite(SPEAKER, LOW);
+
+    //RESET
+    pinMode(RST, INPUT);
+
+
+    //I2C
+    Wire.begin(I2C_SDA, I2C_SCL);
+
+
+    //CAMERA
+    pinMode(RUNCAM, OUTPUT);
+    digitalWrite(BUZZER, HIGH);
+
+    //
+}
