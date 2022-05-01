78638c5870bd0b51ef875aa58c7fbfeccc1ef9e6
Kuba <74020427+JSiuda1@users.noreply.github.com>
2022-05-02 00:23:59 +0200

    .

--- R4v2_Tanwa_COM/include/structs/tanwaStruct.h
+++ R4v2_Tanwa_COM/include/structs/tanwaStruct.h
@@ -1,41 +1,15 @@
-#ifndef STATE_MACHINE_HH
-#define STATE_MACHINE_HH
+#ifndef MAIN_CLASS
+#define MAIN_CLASS
 
 #include "FreeRTOS.h"
-#include "Arduino.h"
+#include <SPI.h>
 
-enum States{
-  INIT = 0,
-  IDLE,
-  ARMED,
-  FUELING,
-  RDY_TO_LAUNCH,
-  COUNTDOWN,
-  FLIGHT,
-  FIRST_STAGE_RECOVERY,
-  SECOND_STAGE_RECOVERY,
-  ON_GROUND,
-  HOLD,
-  ABORT
-};
-
-class StateMachine{
-  static States currentState;
-  static States requestState;
-  static xTaskHandle stateTask;
-  static States holdedState; //keep holded state default is States::HOLD
+class TanWaControl{
 
   public:
-  StateMachine(xTaskHandle _stateTask);
-  static bool changeStateRequest(States _newState);
-  void changeStateConfirmation();
-  void changeStateRejection();
-  States getRequestedState();
-  static States getCurrentState();
+  SPIClass mySPI;
+  
+  TanWaControl();
 };
-
-//static init value
-
-
 
 #endif
