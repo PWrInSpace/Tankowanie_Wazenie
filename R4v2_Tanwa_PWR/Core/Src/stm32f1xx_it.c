3a1e607757117ce96c630d837267abfae314b370
FilipKulisiewicz <73996991+FilipKulisiewicz@users.noreply.github.com>
2022-06-01 18:58:04 +0200

    add reset to HardFault

--- R4v2_Tanwa_PWR/Core/Src/stm32f1xx_it.c
+++ R4v2_Tanwa_PWR/Core/Src/stm32f1xx_it.c
@@ -93,6 +93,8 @@ void HardFault_Handler(void)
   while (1)
   {
     /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+	HAL_Delay(3000);
+	HAL_NVIC_SystemReset();
     /* USER CODE END W1_HardFault_IRQn 0 */
   }
 }
