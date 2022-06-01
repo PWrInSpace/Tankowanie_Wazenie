ac6079c601e73bd5c8549c619c8d8c6677e7b937
FilipKulisiewicz <73996991+FilipKulisiewicz@users.noreply.github.com>
2022-06-01 18:58:21 +0200

    add reset to hardfault

--- R4v2_Tanwa_PWR/Core/Src/main.cpp
+++ R4v2_Tanwa_PWR/Core/Src/main.cpp
@@ -879,7 +879,6 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-	HAL_NVIC_SystemReset();
 	__disable_irq();
   while (1)
   {
